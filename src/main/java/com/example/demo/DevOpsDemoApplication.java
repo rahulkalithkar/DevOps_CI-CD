87da85317c93eb82c17b4d85be5853c3ff5d6fa5
Rahul Kalithkar <rahulkalithkar1996@gmail.com>
2020-07-20 17:44:37 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/DevOpsDemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsDemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(DevOpsDemoApplication.class);
+
+	public static void main(String[] args) {
+		log.info("Main method has been invoked");
+		SpringApplication.run(DevOpsDemoApplication.class, args);
+	}
+
+}
